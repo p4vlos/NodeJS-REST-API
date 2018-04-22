4579e3ceab8b7cc94257a3045d42b79e0c02eacf
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-04-22 14:26:44 +0100

    created a Database using mySQL and connected it with the app.json so I could get results to the API response

--- app.js
+++ app.js
@@ -2,8 +2,43 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
 app.use(morgan('combined'))
+
+app.get('/user/:id', (req, res) => {
+  console.log("Fetching user with id: " + req.params.id)
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'lbta_mysql'
+  })
+
+  const userId = req.params.id
+  const queryString = "SELECT * FROM users WHERE ID = ?"
+  // const queryString = "SELECT * FROM users"
+
+  connection.query(queryString, [userId], (err, rows, fields) => {
+  // connection.query(queryString, (err, rows, fields) => {
+
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      return
+    }
+
+    console.log("I think we fetched users successfully")
+    
+    const users = rows.map((row) => {
+      return {firstName: row.first_name, lastName: row.last_name}
+    })
+
+    res.json(rows)
+  })
+
+  // res.end()
+})
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
