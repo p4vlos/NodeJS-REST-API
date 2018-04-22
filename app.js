2667a268170efdc48054007d47d58d5356e6a4bc
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-04-22 12:54:19 +0100

    created app.js file to setup our app server using express

---
+++ app.js
@@ -0,0 +1,7 @@
+// load our app server using express..
+const express = require('express')
+const app - express()
+
+app.listen(3003, () => {
+  console.log("Server is up and listening on 3003...");
+})
