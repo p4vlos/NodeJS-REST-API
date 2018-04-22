5fbac3ba5aee506f2428496c0c9bd0f0cc947ca5
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-04-22 13:01:24 +0100

    installed nodemon so when we just need register from the weebsite than just closing up the server and runnning the server again to see if everything works fine

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // load our app server using express..
 const express = require('express')
-const app - express()
+const app = express()
 
+app.get("/", (req, res) => {
+  console.log("Responding to root route")
+  res.send("Hello from ROOOOT")
+})
+
+// localhost:3003
 app.listen(3003, () => {
   console.log("Server is up and listening on 3003...");
 })
