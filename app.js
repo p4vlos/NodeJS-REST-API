f1b5a87c48b9450977a06cd8f12e754bc1f4400d
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-04-22 13:06:35 +0100

    responding json

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello from ROOOOT")
 })
 
+app.get("/users", (req, res) => {
+  var user1 = {firstName: "Stephen", lastName: "Curry"}
+  const user2 = {firstName: "Kevin", lastName: "Durant"}
+  res.json([user1, user2])
+
+  // res.send("Nodemon auto updates when I save this file")
+})
+
 // localhost:3003
 app.listen(3003, () => {
   console.log("Server is up and listening on 3003...");
