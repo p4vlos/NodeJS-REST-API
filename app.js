95dbefe7496a4d3bfd5034a01aab6c2c35a0fc0e
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-04-28 15:19:04 +0100

    refactoring code

--- app.js
+++ app.js
@@ -5,104 +5,19 @@ const morgan = require('morgan')
 const mysql = require('mysql')
 
 const bodyParser = require('body-parser')
-
 app.use(bodyParser.urlencoded({extended: false}))
-
 app.use(express.static('./public'))
-
 app.use(morgan('short'))
 
-app.post('/user_create', (req, res) => {
-  console.log("Trying to create a new user")
-  console.log("How to get the data")
+// let's see how to start refactoring our code
+// I'll show you how to use something called a Router
 
-  const firstName = req.body.create_first_name
-  const lastName = req.body.create_last_name
-
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
-  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
-    if (err) {
-      console.log("Failed to insert new user: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("Inserted a new user with id: ", results);
-    res.end()
-  })
-  
-})
-
-function getConnection() {
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'lbta_mysql'
-  })
-}
-
-app.get('/user/:id', (req, res) => {
-  console.log("Fetching user with id: " + req.params.id)
-
-  const connection = getConnection()
-
-  const userId = req.params.id
-  const queryString = "SELECT * FROM users WHERE ID = ?"
-  // const queryString = "SELECT * FROM users"
-
-  connection.query(queryString, [userId], (err, rows, fields) => {
-  // connection.query(queryString, (err, rows, fields) => {
-
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("I think we fetched users successfully")
-    
-    const users = rows.map((row) => {
-      return {firstName: row.first_name, lastName: row.last_name}
-    })
-
-    res.json(rows)
-  })
-
-  // res.end()
-})
+const router = require('./routes/user.js')
+app.use(router)
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
   res.send("Hello from ROOOOT")
-})
-
-app.get("/users", (req, res) => {
-  // var user1 = {firstName: "Stephen", lastName: "Curry"}
-  // const user2 = {firstName: "Kevin", lastName: "Durant"}
-  // res.json([user1, user2])
-
-  // res.send("Nodemon auto updates when I save this file")
-
-  const connection = getConnection()
-
-  const queryString = "SELECT * FROM users"
-
-  connection.query(queryString, (err, rows, fields) => {
-
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("I think we fetched users successfully")
-    
-    const users = rows.map((row) => {
-      return {firstName: row.first_name, lastName: row.last_name}
-    })
-
-    res.json(rows)
-  })
 })
 
 // localhost:3003
